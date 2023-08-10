6a6e499cc6316d4b4eafece3aeba2a820aeab843
JlIlIllIlIIlIll <mohamed@ftouni.me>
2023-08-10 06:51:13 +0300

    Environment setup & featured products

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="border-t bg-slate-50">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs font-semibold text-black">
+          &copy; 2023 Store. Inc, All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
